dec7360fb4928eaa9d9f3594bafb810bcf93d99b
= <dev.258.edilson@gmail.com>
2024-08-13 06:39:56 +0200

    feat: added fn-prototype type

--- src/type.h
+++ src/type.h
@@ -1,3 +1,8 @@
+#ifndef SML_TYPE
+#define SML_TYPE
+
+#include <stddef.h>
+
 typedef enum {
   TYPE_INT = 1,
   TYPE_STR,
@@ -5,3 +10,12 @@ typedef enum {
 
 #define TYPE(t)                                                                \
   ((t == TYPE_INT) ? "int" : (t == TYPE_STR) ? "str" : "UNKNOWN TYPE")
+
+typedef struct {
+  char *name;
+  Type return_type;
+  size_t param_count;
+  Type *param_types;
+} FnPrototype;
+
+#endif
